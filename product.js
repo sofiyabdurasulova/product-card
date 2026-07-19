f7d73539c039925a6c568d78491e1ddbefaa28a9
sofiyabdurasulova <f1kaaa.2ooo@gmail.com>
2026-07-19 05:06:01 +0300

    feature 06 homework

--- product.js
+++ product.js
@@ -1,5 +1,5 @@
-let cart = [];
-let buttons = document.querySelectorAll('.card__add-to-cart');
+const cart = [];
+const buttons = document.querySelectorAll('.card__add-to-cart');
 
 buttons.forEach(function (button) {
   button.addEventListener('click', function () {
@@ -8,3 +8,35 @@ buttons.forEach(function (button) {
     console.log("Товар добавлен в корзину:", cart);
   });
 });
+const mainTitle = document.querySelector('.container__main-title');
+mainTitle.addEventListener('mouseenter', () => {
+  console.log(mainTitle.textContent);
+})
+const card = document.querySelector('.card');
+const toggleCardColor = document.querySelector('.card__buy-button');
+toggleCardColor.addEventListener('click', () => {
+  card.classList.toggle('toggle-card-active');
+})
+const cards = document.querySelectorAll('.card');
+const toggleCardsColor = document.querySelector('.cards-button');
+console.log(toggleCardsColor);
+toggleCardsColor.addEventListener('click', () => {
+  console.log('click!');
+  cards.forEach((card) => card.classList.toggle('toggle-cards-active'));
+})
+const openDokaButton = document.querySelector('.open-doka');
+openDokaButton.addEventListener('click', openDoka)
+function openDoka() {
+  const answer = confirm('Вы действительно хотите открыть Дока?');
+  if (answer === true) {
+    window.open('https://doka.guide');
+  } else {
+    return
+  }
+}
+const outputConsoleButton = document.querySelector('.output-console-log');
+outputConsoleButton.addEventListener('click', () => outputConsoleLOg('дз №6'));
+function outputConsoleLOg(message) {
+  alert(message);
+  console.log(message);
+}
